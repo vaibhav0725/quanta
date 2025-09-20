25534fab365a79a74dffac494e20da7aec3e4207
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-09-20 16:49:56 +0530

    updt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Container } from "@/components/container";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
+    <Suspense>
     <div className="min-h-screen w-full relative">
       <Container className="relative border-dashed border border-neutral-800 w-6xl"/>
-  </div>
+    </div>
+    </Suspense>
   )
 }
